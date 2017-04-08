adb32fdbd9d285f8ba68af790115523f34880b1d
Hamid <hmirzai@gmail.com>
2017-04-08 21:04:28 +0000

    add gpio and timing to rp c++ codes

--- rp/keephistory.sh
+++ rp/keephistory.sh
@@ -1,13 +1,18 @@
 sudo apt-get install emacs24
-sudo apt-get install feh
-sudo apt-get install raspi-gpio
-sudo apt-get install screen
+# sudo apt-get install feh
+# sudo apt-get install raspi-gpio
+# sudo apt-get install screen
 sudo apt-get install libopencv-dev python-opencv
+# sudo apt-get install libav-tools
 git clone git://git.drogon.net/wiringPi
 cd ~/wiringPi
 ./build 
 
 ####
+
+echo alias emacs=\"emacs -nw\" >> ~/.bashrc
+echo sudo modprobe bcm2835-v4l2 >> ~/.bashrc
+
 
 raspistill 
 raspivid -o /dev/null -t 0
@@ -17,4 +22,5 @@ vi .ssh/authorized_keys
 chmod 400 .ssh/authorized_keys 
 wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip
 raspivid -o video.h264 -fps 200 -w 320 -h 240 -t 10000 &
-sudo modprobe bcm2835-v4l2
+avconv -y -r 10 -i image%d.png -r 10 -vcodec mpeg4 -q:v 10 video.mp4
+time sudo ./mountcar
--- rp/mountcar.cpp
+++ rp/mountcar.cpp
@@ -3,6 +3,13 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <algorithm>
+#include <wiringPi.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
 
 using namespace cv;
 using namespace std;
@@ -52,25 +59,82 @@ void getInitPosition(int & x, int & y) {
   }
 }
 
-int main( int argc, char** argv )
-{
+int initCam() {
   cap.set(CV_CAP_PROP_FRAME_WIDTH, width);
   cap.set(CV_CAP_PROP_FRAME_HEIGHT, height);
   cap.set(CV_CAP_PROP_FPS, 200);
-  myfile.open ("out.txt");
-
   if ( !cap.isOpened() )  // if not success, exit program
     {
       cout << "Cannot open the web cam" << endl;
       return -1;
     } 
+}
 
-  int x, y;
+int initPwm() {
+  wiringPiSetupGpio();
+  pinMode(18, PWM_OUTPUT);
+  pinMode(13, PWM_OUTPUT);
+  pwmSetClock(20);
+  pwmSetRange(100);
+  
+  pwmWrite(18, 0);
+  pwmWrite(13, 0);
+}
+
+int goRight() {
+  pwmWrite(18, 88);
+  pwmWrite(13, 0);
+}
+
+int goLeft() {
+  pwmWrite(18, 0);
+  pwmWrite(13, 88);
+}
+
+int stop() {
+  pwmWrite(18, 0);
+  pwmWrite(13, 0);
+}
+
+long getTime()
+{
+  struct timeval start;
+  long mtime, seconds, useconds;
+
+  gettimeofday(&start, NULL);
+  seconds  = start.tv_sec;
+  useconds = start.tv_usec;
+
+  mtime = ((seconds) * 1000 + useconds/1000.0) + 0.5;
+  return mtime;
+}
+
+vector<Mat> mats;
+int main( int argc, char** argv )
+{
+  setpriority(PRIO_PROCESS, 0, -21);
+    
+  initPwm();
+  initCam();
+  
+  myfile.open ("out.txt");
+  
+  int x, y;  
   getInitPosition(x, y);
 
+  long time = getTime();
+  long clock = getTime();
+  int dir = true;
+
   for (int i  = 0; i < 1000; ++i) {
+    if (getTime()-time > 300) {
+      time = getTime();
+      dir ? goRight() : goLeft();
+      dir = !dir;
+    }
     cap.read(imgOriginal);
     Mat img = imgOriginal(Rect(max(x - winSize/2, 0), max(y - winSize/2, 0), min(width - x, winSize), min(height - y, winSize)));
+    mats.push_back(img.clone());
     
     Mat imgHSV;
     //Mat imgLines = Mat::zeros( imgOriginal.size(), CV_8UC3);
@@ -95,12 +159,20 @@ int main( int argc, char** argv )
     }
     x = max(x - winSize/2, 0) + newx;
     y = max(y - winSize/2, 0) + newy;
-    
-    myfile << x << "\t" << y << endl;
+
+    long temp = getTime();
+    myfile << temp -clock << "\t"<< x << "\t" << y << endl;
+    clock = temp;
     // circle(imgLines, Point(x, y), 5, Scalar(0,0,255), 3);    
     // imgOriginal = imgOriginal + imgLines;
     // imwrite( "image" + to_string(i) + ".png", imgOriginal);
   }
   myfile.close();
+  stop();
+
+  for (auto i = mats.begin(); i != mats.end(); ++i) {
+    imwrite( "image" + to_string(i-mats.begin()) + ".png", *i);
+  }
   return 0;
+  
 }
