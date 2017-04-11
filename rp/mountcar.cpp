41dc8326bce8b3223233fa81c55b3ca593df31b2
Hamid <hmirzai@gmail.com>
2017-04-10 18:16:41 -0700

    code cleanup

--- rp/mountcar.cpp
+++ rp/mountcar.cpp
@@ -14,18 +14,31 @@
 using namespace cv;
 using namespace std;
 
-//         0.5356 z^2 - 0.3397 z + 1.131e-16
-// x1_e:  ---------------------------------
-//                z^2 - 1.268 z + 0.4644
+const int width = 640;
+const int height = 178;
+const int winSize = 72;
 
-//            17.04 z^2 - 17.04 z
-// x2_e:  ----------------------
-//           z^2 - 1.268 z + 0.4644
-double l1, l2, ldot1, ldot2;
-int m1, m2;
+const int iLowH = 6;
+const int iHighH = 20;
+
+const int iLowS =84; 
+const int iHighS = 255;
+
+const int iLowV = 20;
+const int iHighV = 255;
+
 int kalman(int m, double & l, double & ldot) {
-  l    = 0.5356 * m - 0.3397 * m1 + 1.131e-16 * m2 + 1.268 * l1 - 0.4644 * l2;
-  ldot = 17.04 * m - 17.04 * m1 + 1.268 * ldot1 - 0.4644 * ldot2;
+  //        0.5356 z^2 - 0.3397 z + 1.131e-16
+  // x1_e:  ---------------------------------
+  //             z^2 - 1.268 z + 0.4644
+  
+  //         17.04 z^2 - 17.04 z
+  // x2_e:  ----------------------
+  //        z^2 - 1.268 z + 0.4644
+  static double l1, l2, ldot1, ldot2;
+  static int m1, m2;
+  l    = 0.5356 * m - 0.3397 * m1 + 1.131e-16 * m2 + 1.268 * l1    - 0.4644 * l2;
+  ldot = 17.040 * m - 17.040 * m1                  + 1.268 * ldot1 - 0.4644 * ldot2;
 
   m2 = m1;
   m1 = m;
@@ -35,57 +48,77 @@ int kalman(int m, double & l, double & ldot) {
   ldot1 = ldot;
 }
 
-const int width = 640;
-const int height = 178;
-const int winSize = 72;
+void processImage(VideoCapture & cap, int & x, int & y, bool windowed, bool fineProc) {
+  Mat imgOriginal;
+  Mat imgHSV;
+  Mat imgThresholded;
+  Mat img;
 
-int goal = -130;
-int totalTime = 10000;
-int maxPwm = 30;
+  if (!windowed) 
+    x = y = 0;
 
-int iLowH = 6;
-int iHighH = 20;
+  cap.read(imgOriginal);
+  if (windowed)
+    img = imgOriginal(Rect(max(x - winSize/2, 0), max(y - winSize/2, 0), min(width - x, winSize), min(height - y, winSize)));
+  else
+    img = imgOriginal;
+    
+  cvtColor(img, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+  inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);  
 
-int iLowS =84; 
-int iHighS = 255;
-
-int iLowV = 20;
-int iHighV = 255;
-
-Mat imgOriginal;
-VideoCapture cap(0); //capture the video from webcam
-ofstream myfile;
-stringstream ss;
-
-void getInitPosition(int & x, int & y) {
-  x = y = 0;
-  while ( x < 10 || y < 10) {
-    cap.read(imgOriginal);
-    Mat imgHSV;
-    cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
-    Mat imgThresholded;
-    inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);  
-      
+  if (fineProc) {
     //morphological opening (removes small objects from the foreground)
     erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-
+    dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+    
     //morphological closing (removes small holes from the foreground)
-    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+    dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
     erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-
     Moments oMoments = moments(imgThresholded);
-
     double dM01 = oMoments.m01;
     double dM10 = oMoments.m10;
     double dArea = oMoments.m00;
-
+    
     x = dM10 / dArea;
     y = dM01 / dArea;
+    return;
+  } else {
+    while (true) {
+      Mat nonZeroCoordinates;
+      findNonZero(imgThresholded, nonZeroCoordinates);
+      int newx = 0;
+      int newy = 0;
+      for (int i = 0; i < nonZeroCoordinates.total(); i++ ) {
+	newx +=  nonZeroCoordinates.at<Point>(i).x;
+	newy +=  nonZeroCoordinates.at<Point>(i).y;
+      }
+      if (nonZeroCoordinates.total() > 0) {
+	newx /= nonZeroCoordinates.total();
+	newy /= nonZeroCoordinates.total();
+	if (windowed) {
+	  x = max(x - winSize/2, 0) + newx;
+	  y = max(y - winSize/2, 0) + newy;
+	} else {
+	  x = newx;
+	  y = newx;
+	}
+	break;
+      } else {
+	cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+	inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);  
+      }
+    }
   }
 }
 
-int initCam() {
+void getInitPosition(VideoCapture & cap, int & x, int & y) {
+  x = y = 0;
+  while ( x < 10 || y < 10) {
+    processImage(cap, x, y, false, true);
+  }
+}
+
+int initCam(VideoCapture & cap) {
   cap.set(CV_CAP_PROP_FRAME_WIDTH, width);
   cap.set(CV_CAP_PROP_FRAME_HEIGHT, height);
   cap.set(CV_CAP_PROP_FPS, 200);
@@ -107,12 +140,12 @@ int initPwm() {
   pwmWrite(13, 0);
 }
 
-int goRight() {
+int goRight(int maxPwm) {
   pwmWrite(13, maxPwm);
   pwmWrite(18, 0);
 }
 
-int goLeft() {
+int goLeft(int maxPwm) {
   pwmWrite(13, 0);
   pwmWrite(18, maxPwm);
 }
@@ -135,13 +168,36 @@ long getTime()
   return mtime;
 }
 
-vector<Mat> mats;
+bool controlLaw(double l, double ldot) { // returns direction
+  if (abs(ldot) < 50) {
+    if (abs(l) < 6) {
+      return false;
+    } else {
+      if (l > 0) {
+	return false;
+      } else {
+	return true;
+      }
+    }
+  } else {
+    return (ldot > 0);
+  }
+}
+
 int main( int argc, char** argv )
 {
-  setpriority(PRIO_PROCESS, 0, -21);
-    
+  int goal = -130;
+  int totalTime = 10000;
+  int maxPwm = 30;
+
+  Mat imgOriginal;
+  VideoCapture cap(0); //capture the video from webcam
+  ofstream myfile;
+  stringstream ss;
+  
+  setpriority(PRIO_PROCESS, 0, -20);
   initPwm();
-  initCam();
+  initCam(cap);
   
   if (argc > 1) {
     goal = atoi(argv[1]);
@@ -154,95 +210,33 @@ int main( int argc, char** argv )
   }
 
   int x, y;  
-  getInitPosition(x, y);
-  
-
+  getInitPosition(cap, x, y);
   long time = getTime();
   long clock = getTime();
-  int dir = true;
+  bool dir = true;
   int counter = 0;
 
   for (int i  = 0; i < totalTime; ++i) {
-    cap.read(imgOriginal);
-    Mat img = imgOriginal(Rect(max(x - winSize/2, 0), max(y - winSize/2, 0), min(width - x, winSize), min(height - y, winSize)));
-    // mats.push_back(img.clone());
+    processImage(cap, x, y, true, false);
     
-    Mat imgHSV;
-    cvtColor(img, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
- 
-    Mat imgThresholded;
-
-    inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);  
-      
-    Mat nonZeroCoordinates;
-    findNonZero(imgThresholded, nonZeroCoordinates);
-    int newx = 0;
-    int newy = 0;
-    for (int i = 0; i < nonZeroCoordinates.total(); i++ ) {
-      newx +=  nonZeroCoordinates.at<Point>(i).x;
-      newy +=  nonZeroCoordinates.at<Point>(i).y;
-    }
-    if (nonZeroCoordinates.total() > 0) {
-      newx /= nonZeroCoordinates.total();
-      newy /= nonZeroCoordinates.total();
-      x = max(x - winSize/2, 0) + newx;
-      y = max(y - winSize/2, 0) + newy;
-    } else {
-      Mat imgHSV;
-      cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
- 
-      Mat imgThresholded;
-      inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);  
-      
-      Mat nonZeroCoordinates;
-      findNonZero(imgThresholded, nonZeroCoordinates);
-      int newx = 0;
-      int newy = 0;
-      for (int i = 0; i < nonZeroCoordinates.total(); i++ ) {
-	newx +=  nonZeroCoordinates.at<Point>(i).x;
-	newy +=  nonZeroCoordinates.at<Point>(i).y;
-      }
-      if (nonZeroCoordinates.total() > 0) {
-	newx /= nonZeroCoordinates.total();
-	newy /= nonZeroCoordinates.total();
-	x = newx;
-	y = newy;
-      }
-    }
-
     long temp = getTime();
     double mult = 1;
     double l, ldot;
 
     kalman(1.07 * x - 394, l, ldot);
-    if (abs(ldot) < 50) {
-      if (abs(l) < 6) {
-	dir = false;
-      } else {
-	if (l > 0) {
-	  dir = false;
-	} else {
-	  dir = true;
-	}
-      }
-    } else {
-      dir = (ldot > 0);
-    }
-
-    if (l <= goal)  break;
-    dir ? goRight() : goLeft();
+    dir = controlLaw(l, ldot);
+    dir ? goRight(maxPwm) : goLeft(maxPwm);
 
     ss << temp -clock << "\t"<< x  << "\t" << 1.07 * x - 394 << "\t" << l << "\t" << ldot << "\t" << (int)dir*100 << endl;
     clock = temp;
+
+    if (l <= goal)  break;
   }
   myfile.open ("out.txt");
   myfile << ss.rdbuf();
   myfile.close();
   stop();
 
-  // for (auto i = mats.begin(); i != mats.end(); ++i) {
-  //   imwrite( "image" + to_string(i-mats.begin()) + ".png", *i);
-  // }
   return 0;
   
 }
